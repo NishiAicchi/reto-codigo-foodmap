285b97882d959e5f172c1e4566f74014d164f7b0
NishiAicchi <ai.espichan@gmail.com>
2018-01-02 12:02:15 -0500

    añadiendo evento a SEARCHBAR

--- js/home.js
+++ js/home.js
@@ -1,3 +1,12 @@
 $(document).ready(function() {
+  $searchBar = $('#search-input');
+  $searchBtn = $('#search-btn');
+  $searchResult = $('#search-result');
+
+  // que aparezcan las imagenes al hacer click en el input
+  $searchBar.on('click', function() {
+    $searchResult.removeClass('hide');
+  });
+
   
 });
